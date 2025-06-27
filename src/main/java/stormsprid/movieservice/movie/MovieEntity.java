7cbb2c7fc756b0013af27c9b88c41d5534492c8e
Emil Galiyev <emil333.mem@gmail.com>
2025-06-27 19:13:31 +0500

    feat:add db connection

--- src/main/java/stormsprid/movieservice/movie/MovieEntity.java
+++ src/main/java/stormsprid/movieservice/movie/MovieEntity.java
@@ -1,4 +1,43 @@
 package stormsprid.movieservice.movie;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Year;
+import java.util.List;
+
+//Movie: id, title, description, year, genre, actors, posterUrl
+@Entity
+@Getter
+@Setter
+@Table(name = "movie")
 public class MovieEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+    private Integer year;
+    private String genre;
+    @ElementCollection(fetch = FetchType.EAGER)//отдельная таблица для актеров
+    @Table(name="actors")
+    List<String> actors;
+    String posterUrl;
+
+
+    @Override
+    public String toString() {
+        return "MovieEntity{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", year=" + year +
+                ", genre='" + genre + '\'' +
+                ", actors=" + actors +
+                ", posterUrl='" + posterUrl + '\'' +
+                '}';
+    }
 }
